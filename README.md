169d72f146f5b37ca5284f67ab664c5af3d234e4
Shaon Majumder <shaon.majumder@portonics.com>
2024-08-05 02:18:22 +0600

    v1

---
+++ README.md
@@ -0,0 +1,124 @@
+# Google Sheets Sync Python Package
+
+## Overview
+
+The `google_sheets_crud` Python package provides a simple and efficient way to perform CRUD (Create, Read, Update, Delete) operations on Google Sheets. This package is designed for users who want to interact with Google Sheets programmatically, with support for reading, appending, prepending, updating, and deleting rows. It also supports creating new sheets and spreadsheets, as well as inserting data into them.
+
+---
+
+## Features
+
+- **Read Data**: Retrieve data from a specified sheet.
+- **Append Row**: Add a new row to the end of a sheet.
+- **Prepend Row**: Insert a new row at the top of the sheet.
+- **Update Row**: Modify data in a specific row.
+- **Delete Row**: Remove a row from the sheet.
+- **Delete and Shift Up**: Remove a row and shift subsequent rows up.
+- **Create Spreadsheet**: Create a new spreadsheet and optionally populate it with data.
+- **Create Sheet**: Create a new sheet in an existing spreadsheet.
+- **Create Sheet and Insert Data**: Create a new sheet and insert data into it.
+
+---
+
+## Installation
+
+1. **Install the Package**:
+
+   First, clone the repository or download the package files. Navigate to the package directory where `setup.py` is located and install the package using:
+
+   ```sh
+   python setup.py install
+   ```
+
+​	Alternatively, if you want to install it in editable mode for development:
+
+    ```sh
+    python setup.py develop
+    ```
+
+2. **Dependencies**:
+
+    Ensure you have the following Python packages installed:
+
+    google-auth
+    google-auth-oauthlib
+    google-api-python-client
+    python-dotenv
+
+    Install these dependencies with:
+    ```sh
+    pip install google-auth google-auth-oauthlib google-api-python-client python-dotenv
+    ```
+
+## Usage
+
+1. **Setup**:
+
+    Create a `.env` file in your project directory with the following content:
+    ```sh
+    CREDENTIALS_FILE=your_credential_file.json
+    SPREADSHEET_ID=your_spreadsheet_id
+    ```
+    Replace your_credential_file.json and your_spreadsheet_id with actual Data.
+2. **Script Example**:
+
+    Use the GoogleSheetsCRUD class to perform operations on your Google Sheet. Below is an example script demonstrating how to use the package:
+
+    ```sh
+    from google_sheets_crud import GoogleSheetsCRUD
+
+    def main():
+        # Initialize the CRUD class
+        sheets_crud = GoogleSheetsCRUD()
+
+        # Read data from the sheet
+        print("Reading data...")
+        data = sheets_crud.read_sheet("testsheet")
+        print("Current data in the sheet:")
+        for row in data:
+            print(row)
+
+        # Append a new row
+        print("\nAppending a new row...")
+        append_response = sheets_crud.append_row(["New", "Data"], "testsheet")
+        print(f"Append response: {append_response}")
+
+        # Prepend a new row
+        print("\nPrepending a new row...")
+        prepend_response = sheets_crud.prepend_row(["Prepended", "Data"], "testsheet")
+        print(f"Prepend response: {prepend_response}")
+
+        # Update a specific row
+        print("\nUpdating row 2...")
+        update_response = sheets_crud.update_row(2, ["Updated", "Data"], "testsheet")
+        print(f"Update response: {update_response}")
+
+        # Delete a specific row
+        print("\nDeleting row 3...")
+        sheets_crud.delete_row(3, "testsheet")
+
+        # Delete and shift rows up
+        print("\nDeleting row 2 and shifting rows up...")
+        sheets_crud.delete_row_and_shift_up(2, "testsheet")
+
+    if __name__ == "__main__":
+        main()
+
+    ```
+
+3. **Running the Script**:
+
+    Execute the script using:
+
+    ```sh
+    python use_google_sheets.py
+    ```
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## Contributing
+Contributions are welcome! Please open an issue or submit a pull request on the GitHub repository.
+
+## Contact
+For any questions or feedback, please contact smazoomder@gmail.com .
---
+++ google_sheets_sync.py
@@ -0,0 +1,215 @@
+import os
+from dotenv import load_dotenv
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+load_dotenv()  # Load environment variables from .env file
+
+class GoogleSheetsCRUD:
+    SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+    CREDENTIALS_FILE = os.getenv('CREDENTIALS_FILE')
+    SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
+
+    def __init__(self):
+        self.creds = self.getUserCredentials()
+        self.service = build("sheets", "v4", credentials=self.creds)
+
+    def getUserCredentials(self):
+        creds = None
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", self.SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.CREDENTIALS_FILE, self.SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+        return creds
+
+    def createSpreadsheet(self, title, data=None):
+        """Create a new sheet and optionally populate it with data."""
+        try:
+            # Create the new sheet
+            spreadsheet = {
+                "properties": {"title": title},
+                "sheets": [{"properties": {"title": title}}]
+            }
+            response = self.service.spreadsheets().create(
+                body=spreadsheet,
+                fields="spreadsheetId"
+            ).execute()
+
+            spreadsheet_id = response.get("spreadsheetId")
+            print(f"Created new spreadsheet with ID: {spreadsheet_id}")
+
+            # If data is provided, populate the new sheet
+            if data:
+                self.append_row_to_sheet(spreadsheet_id, title, data)
+
+            return spreadsheet_id
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+            return None
+
+    def createSheet(self, sheet_name):
+        """Create a new sheet in an existing spreadsheet."""
+        try:
+            # Define the request to add a new sheet
+            requests = [{
+                "addSheet": {
+                    "properties": {
+                        "title": sheet_name
+                    }
+                }
+            }]
+
+            # Send the request to the API
+            response = self.service.spreadsheets().batchUpdate(
+                spreadsheetId=self.SPREADSHEET_ID,
+                body={"requests": requests}
+            ).execute()
+
+            # Extract the sheet ID from the response
+            sheet_id = response['replies'][0]['addSheet']['properties']['sheetId']
+            
+            print(f"Sheet '{sheet_name}' created with ID: {sheet_id}")
+            return sheet_id
+
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+            return None
+    
+    def createSheetAndInsertData(self, sheet_name, data):
+        """Create a new sheet and insert data into it."""
+        try:
+            self.createSheet( sheet_name )
+            self.insertData(sheet_name, data)
+
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+            
+    def insertData(self, sheet_name, data):
+        try:
+            values = self.readSheet(sheet_name)
+            start_row = len(values)+1
+            range_name = f"{sheet_name}!A"+str(start_row)
+            
+            # Insert data into the new sheet
+            response = self.service.spreadsheets().values().update(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=range_name,
+                valueInputOption="RAW",
+                body={"values": data}
+            ).execute()
+
+            print(f"Data inserted into sheet '{sheet_name}'.")
+            print(f"Update response: {response}")
+
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+
+    def readSheet(self, sheet_name):
+        try:
+            result = self.service.spreadsheets().values().get(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=sheet_name
+            ).execute()
+            values = result.get("values", [])
+            return values
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+            return None
+
+    def appendRow(self, row_data, sheet_name):
+        try:
+            response = self.service.spreadsheets().values().append(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=sheet_name,
+                valueInputOption="RAW",
+                body={"values": [row_data]}
+            ).execute()
+            return response
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+            return None
+
+    def prependRow(self, new_row, sheet_name):
+        try:
+            values = self.read_sheet(sheet_name)
+            updated_values = [new_row] + values
+            range_to_update = f"{sheet_name}!A1:Z"
+            result = self.service.spreadsheets().values().update(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=range_to_update,
+                valueInputOption="RAW",
+                body={"values": updated_values}
+            ).execute()
+            print("New row prepended successfully.")
+            print(f"Update response: {result}")
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+
+    def updateRow(self, row_index, row_data, sheet_name):
+        try:
+            range_name = f"{sheet_name}!A{row_index}:Z{row_index}"
+            response = self.service.spreadsheets().values().update(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=range_name,
+                valueInputOption="RAW",
+                body={"values": [row_data]}
+            ).execute()
+            return response
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+            return None
+
+    def deleteRow(self, row_index, sheet_name):
+        try:
+            values = self.read_sheet(sheet_name)
+            if not values or row_index > len(values):
+                print("Row index out of range.")
+                return
+            clear_range = f"{sheet_name}!A{row_index}:Z{row_index}"
+            result = self.service.spreadsheets().values().clear(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=clear_range
+            ).execute()
+            print(f"Row {row_index} deleted.")
+        except HttpError as err:
+            print(f"An error occurred: {err}")
+
+    def deleteRowAndShiftUp(self, row_index, sheet_name):
+        try:
+            values_original = self.read_sheet(sheet_name)
+            length_data = len(values_original)
+            if not values_original or row_index > length_data:
+                print("Row index out of range.")
+                return
+            clear_range = f"{sheet_name}!A{row_index}:Z{row_index}"
+            result = self.service.spreadsheets().values().clear(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=clear_range
+            ).execute()
+            values_to_update = values_original[row_index:]
+            range_to_update = f"{sheet_name}!A{row_index}:Z"
+            self.service.spreadsheets().values().update(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=range_to_update,
+                valueInputOption="RAW",
+                body={"values": values_to_update}
+            ).execute()
+            clear_range = f"{sheet_name}!A{length_data}:Z{length_data}"
+            result = self.service.spreadsheets().values().clear(
+                spreadsheetId=self.SPREADSHEET_ID,
+                range=clear_range
+            ).execute()
+            print(f"Row {row_index} deleted and rows shifted up.")
+        except HttpError as err:
+            print(f"An error occurred: {err}")
